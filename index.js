deadaaf945479301c0b7a982843fec9acab580b0
richie20502 <richie.lugo.recillas.1990@gmail.com>
2024-02-24 00:43:04 -0600

    cambios de cors

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const bodyParser = require('body-parser');
 const sequelize = require('./config/sequelizeConfig');
-//app.use(cors());
-app.use(bodyParser.json());
+app.use(cors());
 
 
 const port = process.env.PORT || 3000;
